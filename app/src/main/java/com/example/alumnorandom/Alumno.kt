79935eb38207c43dba10955d5ac9bc973a637091
SrFail <ivanbermejomelia@gmail.com>
2025-10-23 10:42:07 +0200

    Creación de la data class Alumno.kt

--- app/src/main/java/com/example/alumnorandom/Alumno.kt
+++ app/src/main/java/com/example/alumnorandom/Alumno.kt
@@ -3,5 +3,8 @@ package com.example.alumnorandom
 data class Alumno (
     val nombre: String,
     val apellido: String,
-    val seleccionado: Boolean = false,
-)
+    var seleccionado: Boolean = false,
+    val alumosSeleccionados: List<Alumno>
+){
+    fun seleccionarAlumnos(){seleccionado = true}
+}
